ae0628b0e581cd70ea8afc991a2375b7f3c8bd80
Harman singh <pb65.harman@gmail.com>
2021-11-26 15:07:36 +0530

    added navbar and forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import BookForm from "./components/BookForm/BookForm";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Login} />
+        {/* <Route exact path="/register" component={Register} /> */}
+        <Route exact path="/home" component={BookForm} />
+
+        {/* <Navbar /> */}
+        {/* <BookForm /> */}
       </Switch>
     </BrowserRouter>
   );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import './Login.css';
 
 const Login = () => {
   return (
-    <div className="container">
-      <h1>Login</h1>
-    </div>
+    <>
+      <div className="main-w3layouts wrapper">
+        <h1>SignUp Here</h1>
+        <div className="main-agileinfo">
+          <div className="agileits-top">
+            <form action="#" method="post">
+              <input className="text" type="text" name="Username" placeholder="Username" required />
+              <input className="text email" type="email" name="email" placeholder="Email" required />
+              <input className="text" type="password" name="password" placeholder="Password" required />
+              <input className="text w3lpass" type="password" name="password" placeholder="Confirm Password" required />
+              <div className="wthree-text">
+
+                <div className="clear"> </div>
+              </div>
+              <input type="submit" defaultValue="SIGNUP" />
+            </form>
+            <p>Don't have an Account? <a href="/#"> Login Now!</a></p>
+          </div>
+        </div>
+        <ul className="colorlib-bubbles">
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
+      </div>
+    </>
   );
 };
 
